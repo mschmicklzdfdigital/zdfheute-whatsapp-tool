db1e9bf496098c862008d68152d7ad6016a2891c
mschmicklzdfdigital <matthias.schmickl@zdf.digital>
2026-05-29 20:32:42 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,10 +12,7 @@ class ZDFheuteScraper:
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        # Dieser Pfad ist der Standard-Speicherort für Chromium in der Streamlit-Cloud
         options.binary_location = "/usr/bin/chromium" 
-        
-        # Initialisierung ohne webdriver-manager, da Chromium systemweit vorinstalliert ist
         self.driver = webdriver.Chrome(options=options)
         self.db = DatabaseManager()
 
@@ -23,17 +20,13 @@ class ZDFheuteScraper:
         try:
             self.driver.get("https://www.zdfheute.de/suche?q=*&type=article")
             wait = WebDriverWait(self.driver, 10)
-            
             for _ in range(max_clicks):
                 try:
-                    # Suche den "Mehr laden"-Button
                     btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.button-load-more")))
                     btn.click()
                     time.sleep(2)
-                except Exception:
-                    break # Button nicht mehr gefunden, Ende des Scrappings
+                except: break
             
-            # Daten extrahieren
             elements = self.driver.find_elements(By.CSS_SELECTOR, "div.teaser-standard__body")
             articles = []
             for el in elements:
@@ -41,12 +34,7 @@ class ZDFheuteScraper:
                     title = el.find_element(By.TAG_NAME, "h3").text
                     url = el.find_element(By.TAG_NAME, "a").get_attribute("href")
                     articles.append({'title': title, 'url': url, 'category': 'Sonstige'})
-                except:
-                    continue
-            
-            # In DB speichern
-            if articles:
-                self.db.add_articles(articles)
-                
+                except: continue
+            self.db.add_articles(articles)
         finally:
             self.driver.quit()
